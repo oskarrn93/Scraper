e5abf7d8c8cd1f784cf9e81216bd40e0342100f9
Oskar Rosén <oskar@oskarrosen.com>
2020-09-19 03:38:27 +0200

    refactor tvmatchen scraper to typescript

---
+++ src/scrapers/tvmatchen.ts
@@ -0,0 +1,74 @@
+import axios from 'axios'
+import cheerio from 'cheerio'
+import crypto from 'crypto'
+import parse from 'date-fns/parse'
+import addHours from 'date-fns/addHours'
+import { parseFromTimeZone } from 'date-fns-timezone'
+import { Event } from '../interface/Event'
+
+const url = 'https://www.tvmatchen.nu/'
+const teams = [
+  'Real Madrid',
+  'Malmö FF',
+  'Manchester United',
+  'Paris Saint Germain',
+  'FC Bayern München',
+]
+
+export const scrapeTvMatchen = async (DEBUG = false) => {
+  const response = await axios.get(url)
+  const { data } = response
+
+  const parsedGames = parseTvmatchen(data, DEBUG)
+  return parsedGames
+}
+
+const parseTvmatchen = (data: string, DEBUG: boolean) => {
+  const $ = cheerio.load(data)
+
+  const games = $('.match-list > div')
+
+  if (DEBUG) console.log(`games length: ${games.length}`)
+
+  const result: Event[] = games
+    .map((index, game) => {
+      const title = $(game).find('.match-detail h3').first().text().trim()
+
+      if (!teams.some((team) => title.toLowerCase().includes(team.toLowerCase()))) {
+        return null
+      }
+
+      const description = $(game)
+        .find('.match-channels li a img')
+        .map((index, channel) => {
+          return $(channel).attr('alt')?.trim()
+        })
+        .get()
+        .filter((element) => element)
+        .join(', ')
+
+      const time = $(game).find('.match-time').first().text().trim()
+      const date = $(game).parent().parent().data('date')
+
+      const parsedDate = parse(`${date} ${time}`, 'yyyy-MM-dd HH:mm', new Date())
+
+      const start = parseFromTimeZone(parsedDate.toISOString(), {
+        timeZone: 'Europe/Berlin',
+      })
+
+      const end = addHours(start, 2)
+
+      const event: Event = {
+        title,
+        description,
+        start,
+        end,
+      }
+
+      return event
+    })
+    .get()
+    .filter((element) => element !== null)
+
+  return result
+}
