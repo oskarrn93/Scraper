8a9868d7fc0fbb376401f0edbd2710d0d8d93c3f
Oskar Rosén <oskar@oskarrosen.com>
2021-05-07 01:02:50 +0200

    read from .env

--- deployment/bin/deployment.ts
+++ deployment/bin/deployment.ts
@@ -2,6 +2,9 @@
 import 'source-map-support/register'
 import * as cdk from '@aws-cdk/core'
 import { DeploymentStack } from '../lib/deployment-stack'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = new cdk.App()
 new DeploymentStack(app, 'CalendarAPIStack', {
